d230934fb701a7bcd5777e02f566f5bd7a318ed9
danielc518 <farewell518@gmail.com>
2016-02-18 23:26:26 -0500

    Implemented GHeap using tree

--- hw2/GHeap.ml
+++ hw2/GHeap.ml
@@ -1,19 +1,37 @@
-type 'a heap = 'a list (* ANSWER *);;
+type 'a heap = Empty | Node of 'a * 'a heap * 'a heap (* ANSWER *);;
 
-let empty () = [] (* ANSWER *);;
+let empty () = Empty (* ANSWER *);;
 
 let insert x xs =
-	let rec insert_in_order y lst = 
-		match lst with 
-		| [] -> x :: []
-		| hd :: tl -> 
-			if (y < hd) then y :: hd :: tl
-			else hd :: (insert_in_order y tl)
-	in insert_in_order x xs (* ANSWER *);;
+	let rec insert_heap new_value heap = 
+		match heap with 
+		| Empty -> Node(new_value, Empty, Empty)
+		| Node(value, left_node, right_node) -> 
+			if new_value <= value then 
+				let new_left_node = insert_heap value right_node in
+				Node(new_value, new_left_node, left_node)
+			else 
+				let new_left_node = insert_heap new_value right_node in
+				Node(value, new_left_node, left_node)
+	in insert_heap x xs (* ANSWER *);;
 
 let find_min xs = 
-	match xs with
-	| [] -> None
-	| x :: xs -> Some (x, xs) (* ANSWER *);;
+	let rec remove_min xs =
+		match xs with
+		| Empty -> None
+		| Node(value, left_node, Empty) -> Some (value, left_node)
+		| Node(value, Empty, right_node) -> Some (value, right_node)
+		| Node(value, Node(left_val, _, _) as l, Node(right_val, _, _) as r) ->
+			if left_val <= right_val then Some(value, Node(left_val, remove_min l, r))
+			else Some(value, Node(right_val, l, remove_min r))
+	in remove_min xs (* ANSWER *);;
 
-let as_sorted_list xs = xs (* ANSWER *);;
+let as_sorted_list xs = 
+	let rec to_list xs acc =
+		match xs with
+		| Empty -> [] 
+		| Node(_, _, _) as node ->
+			match find_min node with
+			| None -> acc
+			| Some(value, node) -> to_list node (acc @ [find_min node])
+	in to_list xs [];;
